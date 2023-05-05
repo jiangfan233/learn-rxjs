10e2a32c5e9f23cac0e2726b5a1c7e296b0884dc
jiangfan233 <lostwind233@outlook.com>
2023-05-05 11:45:29 +0800

    挺好玩的

--- src/static-operators/repeatWhen.ts
+++ src/static-operators/repeatWhen.ts
@@ -1,7 +1,14 @@
 import { of } from "rxjs/observable/of";
 import { interval } from "rxjs/observable/interval";
-import { repeatWhen } from "rxjs/operators";
+import { delay, repeatWhen } from "rxjs/operators";
 import { Observable, Subscriber, Subscription } from "rxjs";
+import { empty } from "rxjs/observable/empty";
+import { fromEvent } from "rxjs/observable/fromEvent";
+
+
+// of(1).pipe(
+//     repeatWhen((obs$) => obs$.delay(1000))
+// ).subscribe(console.log)
 
 let str = "";
 
@@ -18,26 +25,27 @@ const source$ = Observable.create((observer: Subscriber<string>) => {
     }
 })
 
-const notifier = <T>(notification$: Observable<T>) => {
+const notifier = <T>(notification$: Observable<T>): Observable<T> => {
+    // @ts-ignore
     return interval(2000);
-    // return notification$.delay(3000)
+    // return notification$.delay(3000);
 }
 
-source$.pipe(
-    fakeRepeatWhen(notifier)
-    // repeatWhen(notifier)
-)
-.subscribe(
-    console.log,
-    console.error,
-    () => console.log("done")
-)
+// source$.pipe(
+//     fakeRepeatWhen(notifier)
+//     // repeatWhen(notifier)
+// )
+// .subscribe(
+//     console.log,
+//     console.error,
+//     () => console.log("done")
+// )
 
 
 type Fn = <T>(obs$: Observable<T>) => Observable<T>;
 
 function fakeRepeatWhen<T>(fn: Fn) {
-    const controller$ = fn();
+    const controller$ = fn(of(1));
 
     // fakeRepeatWhen 是一个高阶函数，
     // 这里接受上游的Observable对象，并给下游返回一个新的Observable对象
@@ -46,6 +54,7 @@ function fakeRepeatWhen<T>(fn: Fn) {
         let sub: Subscription;
 
         const nextFunc = () => {
+            // console.log("nextDunc")
             subscription = obs$.subscribe(
                 (value: T) => observer.next(value),
                 (err: Error) => {
@@ -67,10 +76,19 @@ function fakeRepeatWhen<T>(fn: Fn) {
             }
         };
 
+        const errorFunc = <T>(observer: Subscriber<T>) => observer.error; 
+
+        const completeFunc = <T>(observer: Subscriber<T>) => () => {
+            sub && sub.unsubscribe();
+            controller$.subscribe(nextFunc, errorFunc, completeFunc(observer));
+        }
+
+        nextFunc();
+
         sub = controller$.subscribe(
-            (_) => nextFunc(),
-            (err: Error) => observer.error(err),
-            () => observer.complete()
+            nextFunc,
+            errorFunc(observer),
+            completeFunc(observer)
         )
 
         return {
@@ -80,3 +98,14 @@ function fakeRepeatWhen<T>(fn: Fn) {
         }
     })
 }
+
+
+const app = document.querySelector("#app");
+const button = document.createElement("button");
+button.innerHTML = "click"
+app?.appendChild(button);
+
+source$.pipe(
+    // repeatWhen(() => fromEvent(button, "click"))
+    fakeRepeatWhen(() => fromEvent(button, "click"))
+).subscribe(console.log)
