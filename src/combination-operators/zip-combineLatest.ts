54da32ab3d169d2841b7868cb5a96c63565fd70a
jiangfan233 <lostwind233@outlook.com>
2023-05-14 16:25:45 +0800

    combineLatest 缺陷

--- src/combination-operators/zip-combineLatest.ts
+++ src/combination-operators/zip-combineLatest.ts
@@ -26,30 +26,34 @@ import { map, take } from "rxjs/operators";
 
 // combineLatest 第一次需要填充数据集合，因此需要等待所有数据，
 // 后面的每一次只要任意数据源产生数据都会吐出数据集合
-// combineLatest(
-//     timer(500, 1000).pipe(take(3), map(num => String.fromCharCode(num + 65))),
-//     timer(1000, 1000).pipe(take(4))
-// ).subscribe(
-//     console.log,
-//     console.error,
-//     () => console.log("done")
-// )
+// combineLatest 可接受一个函数project，用于处理combineLatest产生的数据集合并将结果传递给下游
 
-const fakeCombineLatest = (...rest: Observable<any>[]) => {
+
+const fakeCombineLatest = (...args: any[]) => {
+    let project :Function | undefined;
+    let rest: Observable<any>[];
+    if(typeof args[args.length - 1] === "function") {
+        project = args[args.length - 1];
+        rest = args.slice(0, -1);
+    } else {
+        rest = args;
+    }
     const values = Array(rest.length).fill(null);
-    const completeArr = Array(rest.length).fill(false);
+    const completedArr = Array(rest.length).fill(false);
 
-    const nextFunc = (val: any, index: number, observer: Subscriber<any[]>) => {
+
+    const nextFunc = (val: any, index: number, observer: Subscriber<any>) => {
         values[index] = val;
+        
         // 这里仅仅验证 values 中数据的长度
         if(values.every(val => val !== undefined && val !== null)) {
-            observer.next(values.slice());
+            observer.next(project ? project.apply(null, values.slice()) : values.slice());
         }
     }
 
     const completeFunc = (index: number, observer: Subscriber<any[]>) => {
-        completeArr[index] = true;
-        if(completeArr.every(isCompleted => isCompleted)) {
+        completedArr[index] = true;
+        if(completedArr.every(isCompleted => isCompleted)) {
             observer.complete()
         }
     }
@@ -57,6 +61,7 @@ const fakeCombineLatest = (...rest: Observable<any>[]) => {
     return Observable.create((observer: Subscriber<any[]>) => {
         rest.forEach((obs$, index) => {
             obs$.subscribe(
+                // 当有多个数据源同时(逻辑上的同时，物理上相差几纳秒)调用 nextFunc时，每一个都会创建一个宏任务
                 val => nextFunc(val, index, observer),
                 error => observer.error(error),
                 () => completeFunc(index, observer),
@@ -67,9 +72,24 @@ const fakeCombineLatest = (...rest: Observable<any>[]) => {
 
 
 // combineLatest(
+// fakeCombineLatest(
+//     timer(500, 1000).pipe(take(3), map(val => String.fromCharCode(val + 65))), 
+//     timer(1000, 1000).pipe(take(4)),
+//     (a: string, b: number) => `item: ${a} -- ${b}`
+// ).subscribe(
+//     console.log,
+//     console.error,
+//     () => console.log("done")
+// )
+
+// combineLatest 小缺陷：
+// 对于同时到来的两个或多个数据，本应该产生一个输出，实际结果却产生了两个或多个，
+// 因为多个数据源同时产生数据，同时产生多个宏任务，因此也就无法使用微任务解决这个缺陷（个人猜测）
 fakeCombineLatest(
-    timer(500, 1000).pipe(take(3), map(val => String.fromCharCode(val + 65))), 
-    timer(1000, 1000).pipe(take(4))
+// combineLatest(
+    timer(0, 1000).pipe(map(val => `${val}a`), take(1)),
+    timer(0, 1000).pipe(map(val => `${val}b`), take(1)),
+    (a: string, b: string) => `item: ${a} -- ${b}`
 ).subscribe(
     console.log,
     console.error,
